008d3f3d744b1d1356f993f1107697976a7bd2cb
Alia Meek <u2106850@warwick.ac.uk>
2025-06-20 16:40:29 +0100

    correct (most) linting suggestions

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-import os
 import subprocess
 
+DEFAULT_STATS = ["JobName", "Partition", "AllocCPUS", "State", "Elapsed"]
 
-stats_dict = {
-    "JobName" : [],
-    "Parition": [],
-    "AllocCPUs": [],
-    "State": [],
-    "Elapsed": []
-}
 
 def retrieve_stats() -> ():
-    process = subprocess.Popen("sacct -u $(whoami) -n --start=1970-01-01 --format=JobName,Partition,AllocCPUS,State,Elapsed", shell=True, stdout=subprocess.PIPE)
+    process = subprocess.Popen(
+        f"sacct -u $(whoami) -n --start=1970-01-01 --format={','.join(DEFAULT_STATS)}",
+        shell=True, stdout=subprocess.PIPE)
+
     stdout, _ = process.communicate()
 
     stdout = stdout.decode().split('\n')[:-1]
 
     jobs_run = len(stdout)
+
+    stats_dict = {key: [] for key in DEFAULT_STATS}
 
     for job in stdout:
         line = job.split()
@@ -25,6 +23,7 @@ def retrieve_stats() -> ():
             stats_dict[key].append(line[i])
             i += 1
 
+    print("Total jobs run: " + jobs_run)
 
 if __name__ == "__main__":
     retrieve_stats()
